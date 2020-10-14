8f58bb974adc0514953dceafd3ba6dde076252aa
dimejismile <42398304+dimejismile@users.noreply.github.com>
2020-10-14 11:41:50 +0100

    New Changes
    Made new changes

---
+++ src/app/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Response } from '@angular/http';
+
+
+@Injectable()
+export class ProductService {
+
+  constructor(private _http:Http) { }
+
+  getAlbum(id) {}
+}
+
+export class ProductService {
+  constructor(albumUrl) {
+    this.albumUrl = '../assets/album.json';
+}
