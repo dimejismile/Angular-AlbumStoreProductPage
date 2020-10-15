079e7321c20f77daa3a6b6f2ceca6872db011677
dimejismile <42398304+dimejismile@users.noreply.github.com>
2020-10-15 13:06:43 +0100

    Part 6

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -17,6 +17,8 @@ export class ProductListComponent implements OnInit {
   }) { }
 
   ngOnInit() {
+    this._productService.getProducts().subscribe{response =>
+    this.products = response};
   }
 
 }
