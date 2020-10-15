e00dbfd79618eca561cda45cb24ab2a61fffad7b
dimejismile <42398304+dimejismile@users.noreply.github.com>
2020-10-15 12:52:55 +0100

    Update product-list.component.ts

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -12,7 +12,7 @@ import { ProductService } from '../product.service';
 export class ProductListComponent implements OnInit {
 
   products: Product[];
-  constructor(private _productService() {
+  constructor(private _productService: ProductService) {
 
   }) { }
 
